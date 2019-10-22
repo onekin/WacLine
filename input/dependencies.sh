90189f5fe7d06238796b346110cef2257e3aac48
Haritz Medina <haritzmedina@gmail.com>
2019-10-22 15:31:21 +0200

    Fixed bug on MacOSX. When right clicking on any element of xfm, ccfm or vdm, eclipse gets frozen.
    We are not sure about it, but probably pure variants starts loading all the files inside output/ directory for internal checkings. As we have a node_modules folder with more than 500MB and 30K files, it tries to analyze all of them. The solution adopted was to move node_modules folder outside the project scope (it is now created in the workspace folder) and links to node_modules folder inside the generated product is temporal, when gulp default is done, it is removed too.

--- input/dependencies.sh
+++ input/dependencies.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 PROJECT_DIR="./node_modules"
-NODE_MODULES_DIR="../node_modules"
+NODE_MODULES_DIR="../../../node_modules"
 if [ -d "$PROJECT_DIR" ]; then
     echo "Nothing to do"
 else
     if [ -d "$NODE_MODULES_DIR" ]; then
         echo "Creating a symbolic link"
-        ln -s ../node_modules ./node_modules
+        ln -s $NODE_MODULES_DIR $PROJECT_DIR
     else
         echo "npm install and creating a symbolic link"
         npm install
-        mv ./node_modules ../node_modules
-        ln -s ../node_modules ./node_modules
+        mv $PROJECT_DIR $NODE_MODULES_DIR
+        ln -s $NODE_MODULES_DIR $PROJECT_DIR
     fi
 fi
 
 echo "Running gulp default"
 
 gulp default
+
+# Remove node_modules link in project (bug with pure::variants in mac when right click on elements in xfm, vdm, ccfm files)
+rm $PROJECT_DIR
