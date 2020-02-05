1f96eb23dff87925cf4de74ba54cede8998f52d7
Haritz Medina <haritzmedina@gmail.com>
2020-02-05 13:13:14 +0100

    Removing symbolic link creation for node_modules in generated products, because npm and gulp are able to retrieve dependencies from parent folder.

--- input/dependencies.ps1
+++ input/dependencies.ps1
@@ -2,13 +2,11 @@ if (Test-Path ./node_modules) {
 	echo "Nothing to do"
 } else {
 	if (Test-Path ../node_modules) {
-		echo "Creating a symbolic link"
-		New-Item -itemtype symboliclink -path . -name node_modules -value ../node_modules
+		echo "No need to create a symbolic link"
 	} else {
 		echo "npm install and creating a symbolic link"
 		npm install
 		mv ./node_modules ../node_modules
-		New-Item -itemtype symboliclink -path . -name node_modules -value ../node_modules		
 	}
 }
 if (Get-Command gulp -errorAction SilentlyContinue) {
