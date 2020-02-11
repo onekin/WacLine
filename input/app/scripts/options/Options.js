959e682087b2cce407514d7c8f2a18e351fe89c5
Haritz Medina <haritzmedina@gmail.com>
2020-02-11 15:38:42 +0100

    Autoopen and api simulation on moodle working

--- input/app/scripts/options/Options.js
+++ input/app/scripts/options/Options.js
@@ -75,6 +75,29 @@ class Options {
       })
     })
     // PVSCL:ENDCOND
+    // PVSCL:IFCOND(MoodleConsumer or MoodleProvider, LINE)
+    chrome.runtime.sendMessage({scope: 'moodle', cmd: 'getMoodleCustomEndpoint'}, (endpoint) => {
+      document.querySelector('#moodleEndpoint').value = endpoint.endpoint
+    })
+
+    chrome.runtime.sendMessage({scope: 'moodle', cmd: 'isApiSimulationActivated'}, (isActivated) => {
+      document.querySelector('#apiSimulationCheckbox').checked = isActivated.activated
+    })
+    document.querySelector('#apiSimulationCheckbox').addEventListener('change', () => {
+      this.updateApiSimulationCheckbox()
+    })
+    document.querySelector('#moodleEndpoint').addEventListener('change', () => {
+      this.updateMoodleEndpoint()
+    })
+    // PVSCL:ENDCOND
+    // PVSCL:IFCOND(MoodleResource, LINE)
+    chrome.runtime.sendMessage({scope: 'moodle', cmd: 'isAutoOpenFilesActivated'}, (isActivated) => {
+      document.querySelector('#autoOpenCheckbox').checked = isActivated.activated
+    })
+    document.querySelector('#autoOpenCheckbox').addEventListener('change', () => {
+      this.updateAutoOpenCheckbox()
+    })
+    // PVSCL:ENDCOND
   }
   // PVSCL:IFCOND(BrowserStorage,LINE)
 
@@ -105,7 +128,7 @@ class Options {
     })
   }
   // PVSCL:ENDCOND
-  // PVSCL:IFCOND(AnnotationServer->pv:SelectedChildren()->pv:Size()>1,LINE)
+  // PVSCL:IFCOND(AnnotationServer->pv:SelectedChildren()->pv:Size()>1, LINE)
 
   setAnnotationServer (annotationServer) {
     chrome.runtime.sendMessage({
@@ -130,6 +153,54 @@ class Options {
       selectedAnnotationServerConfigurationCard.setAttribute('aria-hidden', 'false')
     }
   }
+
+  // PVSCL:IFCOND(MoodleProvider or MoodleConsumer, LINE)
+  updateApiSimulationCheckbox () {
+    let isChecked = document.querySelector('#apiSimulationCheckbox').checked
+    chrome.runtime.sendMessage({
+      scope: 'moodle',
+      cmd: 'setApiSimulationActivation',
+      data: {isActivated: isChecked}
+    }, (response) => {
+      console.debug('Api simulation is updated to: ' + response.activated)
+    })
+  }
+
+  updateMoodleEndpoint () {
+    let value = document.querySelector('#moodleEndpoint').value
+    let isValidUrl = URLUtils.isUrl(value)
+    if (!isValidUrl) {
+      isValidUrl = URLUtils.isUrl(value + '/')
+      if (isValidUrl) {
+        document.querySelector('#moodleEndpoint').value = value + '/'
+      }
+    }
+    if (isValidUrl) {
+      chrome.runtime.sendMessage({
+        scope: 'moodle',
+        cmd: 'setMoodleCustomEndpoint',
+        data: {endpoint: value}
+      }, ({endpoint}) => {
+        console.debug('Endpoint updated to ' + endpoint.endpoint)
+      })
+    } else {
+      Alerts.errorAlert({error: 'URL is malformed'}) // TODO i18n
+    }
+  }
+  // PVSCL:ENDCOND
+
+  // PVSCL:IFCOND(MoodleResource, LINE)
+  updateAutoOpenCheckbox () {
+    let isChecked = document.querySelector('#autoOpenCheckbox').checked
+    chrome.runtime.sendMessage({
+      scope: 'moodle',
+      cmd: 'setAutoOpenFiles',
+      data: {isActivated: isChecked}
+    }, (response) => {
+      console.debug('Api simulation is updated to: ' + response.activated)
+    })
+  }
+  // PVSCL:ENDCOND
 }
 
 module.exports = Options
