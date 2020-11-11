baeeefd02c0c3ea1c724436e8b011cc7a86b8da1
Haritz Medina <haritzmedina@gmail.com>
2020-11-11 14:35:38 +0100

    Fixed #73 for PDFs

--- input/app/scripts/annotationManagement/read/Screenshots.js
+++ input/app/scripts/annotationManagement/read/Screenshots.js
@@ -17,80 +17,81 @@ class Screenshots {
       const currentPage = window.PDFViewerApplication.page
       // Go to first page
       window.PDFViewerApplication.page = 1
-
-      Alerts.loadingAlert({
-        title: 'Please hold on',
-        text: 'We are creating the annotated PDF document (<span></span> of ' + window.PDFViewerApplication.pagesCount + ')',
-        timerIntervalHandler: (swal) => {
-          swal.getContent().querySelector('span').textContent = window.PDFViewerApplication.page
-        }
-      })
-      // Create pdf file
-      const pdf = new JsPDF('p', 'cm', 'a4', true)
-      // Redraw annotations
-      window.abwa.annotationManagement.annotationReader.redrawAnnotations()
-      // Append rubric
-      const criteriaElement = document.querySelector('#buttonContainer')
-      if (criteriaElement) {
-        html2canvas(criteriaElement).then((rubric) => {
-          const offsetWidth = criteriaElement.offsetWidth
-          const offsetHeight = criteriaElement.offsetHeight
-          pdf.addImage(rubric.toDataURL(), 'png', 0, 0, 29 * offsetWidth / offsetHeight, 29)
-        })
-      }
-      // Create promises array
-      const promisesData = [...Array(window.PDFViewerApplication.pagesCount).keys()].map((index) => { return { i: index } })
-      // Page screenshot promise
-      const takePDFPageScreenshot = (d) => {
-        return new Promise((resolve, reject) => {
-          // Go to page
-          window.PDFViewerApplication.page = d.i + 1
+      Alerts.confirmAlert({
+        alertType: Alerts.alertType.warning,
+        title: 'We will create now the PDF.',
+        text: 'Please don\'t interact with the document or highlighter',
+        callback: () => {
+          // Create pdf file
+          const pdf = new JsPDF('p', 'cm', 'a4', true)
           // Redraw annotations
           window.abwa.annotationManagement.annotationReader.redrawAnnotations()
-          setTimeout(() => {
-            html2canvas(document.querySelector('.page[data-page-number="' + (d.i + 1) + '"]'), { scale: 1 }).then((canvas) => {
-              resolve()
-              if (!(d.i === 0 && !_.isElement(criteriaElement))) {
-                pdf.addPage()
-              }
-              pdf.addImage(canvas.toDataURL(), 'png', 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight(), '', 'FAST')
+          // Append rubric
+          const criteriaElement = document.querySelector('#buttonContainer')
+          if (criteriaElement) {
+            html2canvas(criteriaElement).then((rubric) => {
+              const offsetWidth = criteriaElement.offsetWidth
+              const offsetHeight = criteriaElement.offsetHeight
+              pdf.addImage(rubric.toDataURL(), 'png', 0, 0, 29 * offsetWidth / offsetHeight, 29)
             })
-          }, 750)
-        })
-      }
-      // Wait a little bit to draw annotations in first page
-      setTimeout(() => {
-        // Reduce promise chain
-        const promiseChain = promisesData.reduce(
-          (chain, d) => chain.then(() => {
-            return takePDFPageScreenshot(d)
-          }), Promise.resolve([])
-        )
-        // To execute after promise chain is finished
-        promiseChain.then((canvases) => {
-          // Restore previous page and zoom
-          window.PDFViewerApplication.pdfViewer.currentScale = currentScale
-          window.PDFViewerApplication.page = currentPage
-          Alerts.closeAlert()
-          pdf.save('activity.pdf')
-          // Callback
-          if (_.isFunction(callback)) {
-            callback()
           }
-        })
-      }, 3000)
+          // Create promises array
+          const promisesData = [...Array(window.PDFViewerApplication.pagesCount).keys()].map((index) => { return { i: index } })
+          // Page screenshot promise
+          const takePDFPageScreenshot = (d) => {
+            return new Promise((resolve, reject) => {
+              // Go to page
+              window.PDFViewerApplication.page = d.i + 1
+              // Redraw annotations
+              window.abwa.annotationManagement.annotationReader.redrawAnnotations()
+              setTimeout(() => {
+                html2canvas(document.querySelector('.page[data-page-number="' + (d.i + 1) + '"]'), { scale: 1 }).then((canvas) => {
+                  resolve()
+                  if (!(d.i === 0 && !_.isElement(criteriaElement))) {
+                    pdf.addPage()
+                  }
+                  pdf.addImage(canvas.toDataURL(), 'png', 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight(), '', 'FAST')
+                })
+              }, 750)
+            })
+          }
+          // Wait a little bit to draw annotations in first page
+          setTimeout(() => {
+            // Reduce promise chain
+            const promiseChain = promisesData.reduce(
+              (chain, d) => chain.then(() => {
+                return takePDFPageScreenshot(d)
+              }), Promise.resolve([])
+            )
+            // To execute after promise chain is finished
+            promiseChain.then((canvases) => {
+              // Restore previous page and zoom
+              window.PDFViewerApplication.pdfViewer.currentScale = currentScale
+              window.PDFViewerApplication.page = currentPage
+              Alerts.infoAlert({
+                title: 'PDF is created',
+                text: 'You should be prompted to download  the annotated PDF.'
+              })
+              pdf.save('activity.pdf')
+              // Callback
+              if (_.isFunction(callback)) {
+                callback()
+              }
+            })
+          }, 3000)
+        }
+      })
     } else {
+      /*
+      let blob = new Blob([document.body.outerHTML.toString()], { type: 'text/plain;charset=utf-8' })
+      FileSaver.saveAs(blob, 'activity.html')
+      */
       promise = new Promise((resolve) => {
         html2canvas(document.body).then((canvas) => {
           resolve(canvas)
         })
       })
     }
-    promise.then((canvas) => {
-      canvas.toBlob((blob) => {
-        FileSaver.saveAs(blob, 'exam.png')
-      })
-    })
   }
 }
 
